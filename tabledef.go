424335c19b8b425a8ccedf6e037bbf5d167240b6
Ziska Zarkasi <ziska.zarkasyi@indosatooredoo.com>
2022-02-18 08:38:44 +0700

    change TableDef

--- tabledef.go
+++ tabledef.go
@@ -1,11 +1,11 @@
 package store
 
 type TabledDef struct {
-	Schema   string
-	Name     string
-	KeyField string
-	Primary  []string
-	Index    []TableIndex
+	Schema       string
+	Name         string
+	KeyField     string
+	PrimaryField []string
+	Index        []TableIndex
 }
 
 type TableIndex struct {
